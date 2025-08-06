fdd1b3f68b098e90ee2e63590822f6071fc9c86f
Shravanikale <kaleshravani84@gmail.com>
2025-08-06 22:52:28 +0530

    task completed

---
+++ src/main/java/com/tka/service/ShravaniKale.java
@@ -0,0 +1,14 @@
+package com.tka.service;
+
+public class ShravaniKale {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 30;
+		if (a < b) {
+			System.out.println("Hello!");
+		} else {
+			System.out.println("Bye!");
+		}
+	}
+
+}
