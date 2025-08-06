3ab6d0d2429a09564903a530b73fdab04110898e
ram-chadar <javaprojectwork2715@gmail.com>
2025-08-06 11:57:41 +0530

    first commit

---
+++ src/main/java/com/tka/model/UserLogin.java
@@ -0,0 +1,30 @@
+package com.tka.model;
+
+public class UserLogin {
+	
+	private String username;
+	private String password;
+	
+	public UserLogin() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
