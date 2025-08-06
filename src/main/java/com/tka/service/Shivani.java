674bfa8865fe5e47ed4c11fa5598667bf0895634
shivanimagar5 <shivanimagar130@gmail.com>
2025-08-06 21:57:01 +0530

    task complete

---
+++ src/main/java/com/tka/service/Shivani.java
@@ -0,0 +1,15 @@
+package com.tka.service;
+
+public class Shivani {
+
+	public static void main(String[] args) {
+		int a= 10;
+		int b = 20;
+		int sum= a+b;
+		System.out.println("Addition of two nymber is:"+ sum);
+	}
+}
+
+
+   
+      
